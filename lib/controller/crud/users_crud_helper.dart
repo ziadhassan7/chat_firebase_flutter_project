cef422cf4b753ee4b11290374ba3e267a9be8cf2
ziadhassan7 <ziadhassan7@gmail.com>
2022-08-04 23:43:38 +0200

    add delete function, change col id

--- lib/controller/crud/users_crud_helper.dart
+++ lib/controller/crud/users_crud_helper.dart
@@ -9,12 +9,30 @@ class UsersCRUDHelper {
 
   ///----------------------- Add new User Method
   addNewUser(String userName) async{
-    await fireStore.collection('all users').doc(DateTime.now().toString()).set({
+    await fireStore.collection('all users').doc('user $userName').set({
       'username': userName,
 
     });
 
     MessagesCRUDHelper(userName: userName).pushData("Welcome message!");
+
+  }
+
+
+  ///----------------------- Delete a User Method
+  deleteUser(String id) async {
+
+    await fireStore.collection('all users').doc('user $id').delete();
+
+    //iterate over large number of documents and delete it
+    //more information: https://stackoverflow.com/questions/53089517/how-to-delete-all-documents-in-collection-in-firestore-with-flutter
+    final batch = fireStore.batch();
+    var collection = fireStore.collection('user $id');
+    var snapshots = await collection.get();
+    for (var doc in snapshots.docs) {
+      batch.delete(doc.reference);
+    }
+    await batch.commit();
 
   }
 }
